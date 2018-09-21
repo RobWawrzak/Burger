c03ee58af262c52d746c9b6e9877d8c9ee4dfc02
TheBetterMan <Rob.wawrzak@hotmail.com>
2018-09-20 21:53:13 -0400

    server.js and db SQL files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Select the burgers_db
+USE burgers_db;
+-- Insert new rows of data.
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ("hamburger", false),
+  ("cheeseburger", false),
+  ("veggie burger", false);
